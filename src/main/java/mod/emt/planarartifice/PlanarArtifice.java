b0487b3d575086aff5b9fe5ed49e867c9d57a3da
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-02-26 16:45:35 -0600

    Setup base classes

---
+++ src/main/java/mod/emt/planarartifice/PlanarArtifice.java
@@ -0,0 +1,54 @@
+package mod.emt.planarartifice;
+
+import mod.emt.planarartifice.proxy.CommonProxy;
+import mod.emt.planarartifice.registry.CreativeTabsPA;
+import mod.emt.planarartifice.utils.helpers.LogHelper;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(
+        modid = PlanarArtifice.MOD_ID,
+        name = PlanarArtifice.MOD_NAME,
+        version = PlanarArtifice.MOD_VERSION,
+        dependencies = PlanarArtifice.DEPENDENCIES
+)
+public class PlanarArtifice {
+    public static final String MOD_ID = Tags.MOD_ID;
+    public static final String MOD_NAME = "Cursed Artifice";
+    public static final String MOD_VERSION = Tags.VERSION;
+    public static final String DEPENDENCIES = "required-after:thaumcraft";
+
+    public static final String CLIENT_PROXY = "mod.emt.planarartifice.proxy.ClientProxy";
+    public static final String COMMON_PROXY = "mod.emt.planarartifice.proxy.CommonProxy";
+
+    public static final CreativeTabs tabPA = new CreativeTabsPA(CreativeTabs.CREATIVE_TAB_ARRAY.length, "PlanarArtificeTab");
+
+    @Mod.Instance(MOD_ID)
+    public static PlanarArtifice instance;
+
+    @SidedProxy(clientSide = CLIENT_PROXY, serverSide = COMMON_PROXY)
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        LogHelper.info("Starting " + MOD_NAME);
+        proxy.preInit();
+        LogHelper.debug("Finished preInit phase.");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init();
+        LogHelper.debug("Finished init phase.");
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit();
+        LogHelper.debug("Finished postInit phase.");
+    }
+}
