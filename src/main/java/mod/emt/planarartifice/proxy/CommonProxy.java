ed2c1421d2507d9843c40f33e3e91a1b395c7248
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-04-09 00:04:44 -0500

    Add planar infusion enchantments page

--- src/main/java/mod/emt/planarartifice/proxy/CommonProxy.java
+++ src/main/java/mod/emt/planarartifice/proxy/CommonProxy.java
@@ -33,12 +33,13 @@ public class CommonProxy {
     private void registerResearch() {
         ResearchCategories.registerCategory(
                 "PLANAR_ARTIFICE", "FIRSTSTEPS", new AspectList(),
-                new ResourceLocation(PlanarArtifice.MOD_ID, "textures/research/logo_icon.png"),
+                new ResourceLocation(PlanarArtifice.MOD_ID, "textures/misc/logo_icon.png"),
                 new ResourceLocation(PlanarArtifice.MOD_ID, "textures/gui/research_background.jpg"),
                 new ResourceLocation(Thaumcraft.MODID, "textures/gui/gui_research_back_over.png"));
 
         ThaumcraftApi.registerResearchLocation(new ResourceLocation(PlanarArtifice.MOD_ID, "research/alkimium"));
         ThaumcraftApi.registerResearchLocation(new ResourceLocation(PlanarArtifice.MOD_ID, "research/basics"));
         ThaumcraftApi.registerResearchLocation(new ResourceLocation(PlanarArtifice.MOD_ID, "research/bismuth"));
+        ThaumcraftApi.registerResearchLocation(new ResourceLocation(PlanarArtifice.MOD_ID, "research/mirromirous"));
     }
 }
