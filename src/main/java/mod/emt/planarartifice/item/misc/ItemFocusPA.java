4fdf40a9a137915b2fab6869a7a5811bd0a0eff1
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-04-15 17:40:17 -0500

    Fix focus not showing up on creative tab

--- src/main/java/mod/emt/planarartifice/item/misc/ItemFocusPA.java
+++ src/main/java/mod/emt/planarartifice/item/misc/ItemFocusPA.java
@@ -1,7 +1,10 @@
 package mod.emt.planarartifice.item.misc;
 
+import mod.emt.planarartifice.PlanarArtifice;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.common.IRarity;
 import org.jetbrains.annotations.NotNull;
 import thaumcraft.common.items.casters.ItemFocus;
@@ -12,6 +15,15 @@ public class ItemFocusPA extends ItemFocus {
     public ItemFocusPA(String name, int complexity) {
         super(name, complexity);
         this.setTranslationKey(Objects.requireNonNull(this.getRegistryName()).toString());
+        this.setCreativeTab(PlanarArtifice.tabPA);
+    }
+
+    //This is necessary, otherwise it won't show up on the creative tab
+    @Override
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
+        if (tab == PlanarArtifice.tabPA || tab == CreativeTabs.SEARCH) {
+            items.add(new ItemStack(this, 1, 0));
+        }
     }
 
     @Override
